cfd9857a4d8d7f43514a24e16d949bbd7f13b6bc
lhdp0110 <lou-ise@hotmail.fr>
2022-11-17 15:39:58 -0600

    Initializing project files
    Creating separate files for each of the three main steps of project (ode system creation, fitting params to data, sensitivity analysis) and copying code from scripts stored on desktop

---
+++ data_fitting.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 17 15:36:25 2022
+
+@author: perrielh
+"""
+
---
+++ param_sensitivity.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 17 15:37:44 2022
+
+@author: perrielh
+"""
+
